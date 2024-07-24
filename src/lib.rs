1181d1bc2be0a52d224bcc701000997916569b8d
Charlotte Jones <Charlotte.Jones@armedicalboard.org>
2024-07-24 15:46:09 -0500

    Added Get Method

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod filters {
+    use super::handlers;
+    use warp::Filter;
+
+    pub fn list() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("holodeck")
+            .and(warp::get())
+            .and_then(handlers::handle_list)
+    }
+}
+
+mod handlers {
+    use std::convert::Infallible;
+    use warp::http::StatusCode;
+
+    pub async fn handle_list() -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::ACCEPTED)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::filters;
+    use warp::http::StatusCode;
+    use warp::test::request;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        let api = filters::list();
+
+        let response = request().method("GET").path("/holodeck").reply(&api).await;
+
+        assert_eq!(response.status(), StatusCode::ACCEPTED);
     }
 }
